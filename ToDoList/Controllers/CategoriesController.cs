589c09e7c5a7da0b3127fd2c6e7195979b7f1cf8
Brooke Kullberg and Dustin Herboldshimer <dustin@>
2019-05-02 09:25:07 -0700

    refactor the Show() method and its View within CategoriesController to use category object, not dictionary

--- ToDoList/Controllers/CategoriesController.cs
+++ ToDoList/Controllers/CategoriesController.cs
@@ -26,8 +26,11 @@ namespace ToDoList.Controllers
     {
       Category newCategory = new Category(categoryName);
       List<Category> allCategories = Category.GetAll();
-      return View("Index", allCategories);
+      // return View("Index", allCategories);
+      return RedirectToAction("Index");
     }
+    //modified Controller and Show View to accept a single category object
+    //instead of a Dictionary (we thought dictionary was redundant)
 
     [HttpGet("/categories/{id}")]
     public ActionResult Show(int id)
@@ -37,21 +40,21 @@ namespace ToDoList.Controllers
       List<Item> categoryItems = selectedCategory.GetItems();
       model.Add("category", selectedCategory);
       model.Add("items", categoryItems);
-      return View(model);
+      // return View(model);
+      return View(selectedCategory);
     }
-
     // This one creates new Items within a given Category, not new Categories:
     [HttpPost("/categories/{categoryId}/items")]
     public ActionResult Create(int categoryId, string itemDescription)
     {
-      Dictionary<string, object> model = new Dictionary<string, object>();
+      // Dictionary<string, object> model = new Dictionary<string, object>();
       Category foundCategory = Category.Find(categoryId);
       Item newItem = new Item(itemDescription);
       foundCategory.AddItem(newItem);
-      List<Item> categoryItems = foundCategory.GetItems();
-      model.Add("items", categoryItems);
-      model.Add("category", foundCategory);
-      return View("Show", model);
+      // List<Item> categoryItems = foundCategory.GetItems();
+      // model.Add("items", categoryItems);
+      // model.Add("category", foundCategory);
+      return View("Show", foundCategory);
     }
 
   }
